1d613c946e2b0a93a68a88cfdb809d7ad6155034
max54165 <max6894062@gmail.com>
2018-04-20 11:18:52 +0800

    新增一個砲塔 但目歉並不能左右選轉 按左和右 只能單純的往左或往右移動

---
+++ Assets/move.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour {
+    public float speed;
+    Rigidbody rigidbody;
+	// Use this for initialization
+	void Start () {
+        rigidbody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rigidbody.velocity = movement * speed;
+	}
+}
