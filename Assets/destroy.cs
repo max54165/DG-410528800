331e9bc1ad81db62dc2c9bce8f0d65bbacd6137b
max54165 <max6894062@gmail.com>
2018-04-20 11:54:22 +0800

    球射擊方塊時會消失 但由於時間的關西 方塊不能去控制 不過射出去的子彈打得到物品

---
+++ Assets/destroy.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destroy : MonoBehaviour {
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "player")
+        {
+            return;
+        }
+
+        Destroy(other.gameObject);
+        Destroy(gameObject);
+        //gamefunction.Instance.AddScore();
+    }
+}
